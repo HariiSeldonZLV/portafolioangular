09af1d6df1c39b30b5baf067307f7b394bac64a6
Denis Salinas <dje.salinas.m@gmail.com>
2026-03-16 19:11:27 -0300

    incluyendo las llaves solo para deploy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true, // true en prod.ts
+  emailjsServiceId: 'REPLACE_WITH_NETLIFY_VAR',
+  emailjsTemplateId: 'REPLACE_WITH_NETLIFY_VAR',
+  emailjsPublicKey: 'REPLACE_WITH_NETLIFY_VAR'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false, // true en prod.ts
+  emailjsServiceId: 'REPLACE_WITH_NETLIFY_VAR',
+  emailjsTemplateId: 'REPLACE_WITH_NETLIFY_VAR',
+  emailjsPublicKey: 'REPLACE_WITH_NETLIFY_VAR'
+};
